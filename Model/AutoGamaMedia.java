1b8a29825b4e54617c448471ce51fdeac5b88293
MartinianoT <martitrigueiro@gmail.com>
2024-11-16 20:07:51 -0300

    Arreglado

--- Model/AutoGamaMedia.java
+++ Model/AutoGamaMedia.java
@@ -1,9 +1,29 @@
 public class AutoGamaMedia extends Auto{
+
+    private int kilometraje;
+
+    private enum estadoDeCompra;
+
+    private int modelo;
+
+    private String marca;
+
     private int precioBase;
 
-    public AutoGamaMedia() {
-        super();
+    public AutoGamaMedia(int kilometraje, String marca, int modelo, int precioLista, int kilometraje1, int modelo1, String marca1, int precioBase) {
+        super(kilometraje, marca, modelo, precioLista);
+        this.kilometraje = kilometraje1;
+        this.modelo = modelo1;
+        this.marca = marca1;
+        this.precioBase = precioBase;
     }
 
-    public AutoGamaMedia(int kilometraje,)
+    public void intentarComprar(void Cliente) {
+        return null;
+    }
 
+    public boolean es0KM() {
+        return false;
+    }
+
+}
