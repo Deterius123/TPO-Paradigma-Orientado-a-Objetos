377231eda63dc6c39b7c2620bea24128fd7b1e1d
MartinianoT <martitrigueiro@gmail.com>
2024-11-18 17:41:10 -0300

    Arreglado

--- Model/Agencia.java
+++ Model/Agencia.java
@@ -83,8 +83,9 @@ public class Agencia {
     public List<Auto> filtrarAutosPorMarca(String marca) {
         List<Auto> autosPorMarca = new ArrayList<>(); // Lista para autos que sean de la marca que buscamos
         // Si la marca es igual a la buscada se la agrega a la lista
+        // toUpperCase() se asegura que la marca empiece en mayuscula
         for (Auto auto : listaAutos){
-            if (auto.getMarca().equals(marca)){
+            if (auto.getMarca().equals(marca.toUpperCase())){
                 autosPorMarca.add(auto);
             }
         }
